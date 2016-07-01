61160df9429c74bf9c9464b17ea323ed4c94f77c
v.ryabenko <v.ryabenko@aimprosoft.com>
2016-07-01 17:42:07 +0300

    fix sql file

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,2 +1,5 @@
 -- noinspection SqlDialectInspectionForFile
 INSERT INTO _user (id, username, password, enabled) VALUES (1,'admin', '1', true);
+
+INSERT INTO authoritie (id, user_id, authority) VALUES (1,1, 'ROLE_ADMIN');
+INSERT INTO authoritie (id, user_id, authority) VALUES (2,1, 'ROLE_USER');
